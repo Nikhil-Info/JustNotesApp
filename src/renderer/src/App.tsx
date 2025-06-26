3afbbd6e758fe5450f222972ed4c55b74921184c
Nikhil <laudyanikhil@gmail.com>
2025-06-26 02:53:57 -0600

    Update App.tsx

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -21,9 +21,9 @@ const App = () => {
     <>
       <DraggableTopBar />
       <RootLayout>
-        <Sidebar className="p-2">
-          <ActionButtonsRow className="flex justify-between mt-1" />
-          <NotePreviewList className="mt-3 space-y-1" onSelect={resetScroll} />
+        <Sidebar className="p-2 bg-green-900/50">
+          <ActionButtonsRow className="flex justify-between mt-1 " />
+          <NotePreviewList className="mt-3 space-y-1 " onSelect={resetScroll} />
         </Sidebar>
 
         <Content ref={contentContainerRef} className="border-l bg-zinc-900/50 border-l-white/20">
